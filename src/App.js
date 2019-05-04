351f68804f828d087fa6c6b42028323a514cd318
Mariana Silva <marianamvsilva@gmail.com>
2019-05-04 14:31:06 +0100

    Added Chris joke

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Byebye Germany <code /> Hi, React.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Mariana
+          When you are ready to take Chris' job ;)
         </a>
       </header>
     </div>
